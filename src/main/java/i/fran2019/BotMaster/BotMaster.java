c7b53d166427a2b3140a5524f7bb8776a7240a20
iFran2019 <fran1234532@hotmail.com>
2025-04-02 00:58:35 +0200

    1.0.7
    Java 17 Migrated to Java 21
    Updated JDA LIB
    Added @NonNull to some variables

--- src/main/java/i/fran2019/BotMaster/BotMaster.java
+++ src/main/java/i/fran2019/BotMaster/BotMaster.java
@@ -6,6 +6,7 @@ import i.fran2019.BotMaster.Managers.CommandManager;
 import i.fran2019.BotMaster.Managers.ConfigManager;
 import i.fran2019.BotMaster.Managers.PluginManager;
 import lombok.Getter;
+import lombok.NonNull;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.exceptions.InvalidTokenException;
@@ -16,14 +17,14 @@ import org.slf4j.LoggerFactory;
 import redis.clients.jedis.UnifiedJedis;
 
 public class BotMaster {
-    @Getter private static Logger logger = LoggerFactory.getLogger(BotMaster.class);
-    @Getter private static BotMaster botMaster;
+    @NonNull @Getter private static Logger logger = LoggerFactory.getLogger(BotMaster.class);
+    @NonNull @Getter private static BotMaster botMaster;
     @Getter private CommandManager commandManager;
     @Getter private PluginManager pluginManager;
     @Getter private MongoClient mongoClient;
     @Getter private UnifiedJedis redisClient;
     @Getter private ConfigManager configManager;
-    @Getter private JDA jda;
+    @NonNull @Getter private JDA jda;
 
     public static void main(String[] args) {
         botMaster = new BotMaster();
@@ -37,8 +38,6 @@ public class BotMaster {
         this.configManager = new ConfigManager();
         this.mongoClient = this.configManager.MONGODB_ENABLED ? MongoClients.create(this.configManager.MONGODB_URI) : null;
         this.redisClient = this.configManager.REDIS_ENABLED ? new UnifiedJedis(this.configManager.REDIS_URI) : null;
-        botMaster.build();
-
         this.commandManager = new CommandManager();
         this.pluginManager = new PluginManager();
     }
